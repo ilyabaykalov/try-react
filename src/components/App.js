f46525bf3ec7066c44d57232d0f38f34c978b622
Baykalov Ilya <enot_boris@icloud.com>
2020-01-20 11:11:56 +0700

    test eslint

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,17 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import "../styles/App.scss";
+import '../styles/App.scss';
+
+let testVariable;
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>My React App!</h1>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<h1>My React App!</h1>
+			</div>
+		);
+	}
 }
 
 export default App;
