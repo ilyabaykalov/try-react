a732b36c92d17ba3149076b70cec9f3d3f5b86fc
Baykalov Ilya <enot_boris@icloud.com>
2020-01-21 11:24:31 +0700

    test url-loader

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 
 import './App.scss';
+import test from './test.jpg';
 
 class App extends Component {
 	render() {
 		return (
 			<div>
 				<h1>My React App!</h1>
-				<img src="/src/public/images/test.jpg" alt="test" width={'100px'} />
+				<img src={test} alt={'test'} width={'100px'} />
 			</div>
 		);
 	}
--- webpack.config.js
+++ webpack.config.js
@@ -2,32 +2,37 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: './src/index.js',
-	output: {
-		path: path.join(__dirname, '/dist'),
-		filename: 'index-bundle.js',
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: ['babel-loader'],
-			},
-			{
-				test: /\.s*[ac]ss$/i,
-				use: ['style-loader', 'css-loader', 'sass-loader'],
-			},
-			{
-				test: /\.(png|svg|jpg|gif)$/,
-				use: ['file-loader'],
-			},
-		],
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './src/index.html',
-			favicon: './src/favicon.ico',
-		}),
-	],
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, '/prod'),
+        filename: 'index-bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.s*[ac]ss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        mimetype: 'image/png',
+                    },
+                }],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            favicon: './src/favicon.ico',
+        }),
+    ],
 };
